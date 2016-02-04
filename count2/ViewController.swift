53e0d93983aaa5f0d814f53552d8189742562375
Yuki Sakata <YukiSakata@sakatayuuki-no-macbook-pro.local>
2016-02-04 17:11:55 +0900

    Initial Commit

---
+++ count2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  count2
+//
+//  Created by Yuki Sakata on 2016/02/04.
+//  Copyright © 2016年 Yuki Sakata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
