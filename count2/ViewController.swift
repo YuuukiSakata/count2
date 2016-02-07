7d6a506c701bbfa4ae0ac26522b2aadbd8b340b2
Yuki Sakata <doragon3.0920@gmail.com>
2016-02-07 14:41:00 +0900

    firstcommit

--- count2/ViewController.swift
+++ count2/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+}
+
+    @IBOutlet var myImageView: UIImageView!
+    
+    var myArray: [UIImage]!
+    
+    myArray = [UIImage(named: "golf.jpg")!,
+    UIImage(named: "tennis.jpg")!,
+    UIImage(named: "baseball.jpg")!
+    ]
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number = number -1
+        label.text = String(number)
     }
 
 
+    if number == 43 {
+        myImageView.image = myArray[1]
+    }else if number == 65{
+        myImageView.image = myArray[2]
+    }else if number == 75{
+        myImageView.image = myArray[3]
+    }
+    
 }
-
